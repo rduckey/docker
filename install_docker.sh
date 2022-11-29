fd2857be0e147b07856d49b5d157987488417be2
rduckey <51416046+rduckey@users.noreply.github.com>
2022-11-29 03:06:07 -0500

    Updated to current version of docker compose

--- install_docker.sh
+++ install_docker.sh
@@ -11,7 +11,7 @@ sudo apt install docker-ce
 
 #Docker Compose
 mkdir -p ~/.docker/cli-plugins/
-curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+curl -SL https://github.com/docker/compose/releases/download/v2.13.0/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
 chmod +x ~/.docker/cli-plugins/docker-compose
 docker-compose --version
 
