55dc7053d7465ceb65fe0ba2e2751eca87b8b40e
rduckey <williampayne_amt@live.com>
2022-11-29 03:39:42 -0500

    automated install

--- install_docker.sh
+++ install_docker.sh
@@ -3,11 +3,11 @@
 
 #Docker
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
-sudo apt install docker-ce
+sudo apt install docker-ce -y
 
 #Docker Compose
 mkdir -p ~/.docker/cli-plugins/
