556222fd2a634b4b8a184d0503d91584668a0c2b
rduckey <williampayne_amt@live.com>
2022-11-29 17:54:18 -0500

    Ubuntu installer update
    added conformation messages after installation

--- ubuntu_installer.sh
+++ ubuntu_installer.sh
@@ -8,11 +8,23 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt install docker-ce -y
+sudo usermod -aG docker ${USER}
 
 #Docker Compose
 mkdir -p ~/.docker/cli-plugins/
 curl -SL https://github.com/docker/compose/releases/download/v2.13.0/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
 chmod +x ~/.docker/cli-plugins/docker-compose
-docker-compose --version
 
+docker --version
+if [$? -eq 0] then
+    echo "Docker has been sucessfully installed"
+else
+    echo "Docker has failed to install"
+fi
+docker-compose --version
+if [$? -eq 0] then
+    echo "Docker-Compose has been sucessfully installed"
+else
+    echo "Docker-Compose has failed to install"
+fi
 
