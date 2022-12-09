b1b4af1c06e4da0a8daab16caeaf0bfc267339b2
rduckey <51416046+rduckey@users.noreply.github.com>
2022-12-09 13:12:36 -0500

    uncomment
    uncommented code that was disabled for testing purposes

--- install/ubuntu22.04.sh
+++ install/ubuntu22.04.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #Install docker and docker compose on Ubuntu 22.04
 #Docker
-#sudo apt update
-#sudo apt install ca-certificates curl gnupg lsb-release -y
-#sudo mkdir -p /etc/apt/keyrings
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-#echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-#sudo apt-get update
-#if [ $? != 0 ]; then
-#    sudo chmod a+r /etc/apt/keyrings/docker.gpg
-#    sudo apt-get update
-#fi
-#sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo apt update
+sudo apt install ca-certificates curl gnupg lsb-release -y
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+if [ $? != 0 ]; then
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+    sudo apt-get update
+fi
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo usermod -aG docker "${USER}"
 #Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.14.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
